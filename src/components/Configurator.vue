b3f5f08a1b056ab06ee59c503ca6349da960b2b0
Alec Vercruysse <vercruysse.alec@gmail.com>
2020-06-21 14:15:38 -0700

    Boilerplate for initializing app with repo contents

---
+++ src/components/Configurator.vue
@@ -0,0 +1,41 @@
+<script>
+import axios from 'axios';
+
+ export default {
+     name: "Configurator",
+     data: function() {
+         return {
+             repo: "AlecVercruysse/UC2-GIT",
+             contents: {}
+         }
+     },
+     methods: {
+         getConfig() {
+             var that = this;
+             const url = "https://api.github.com/repos/" + this.repo + "/contents"
+             axios.get(url).then(function (response) {
+                 console.log(response.data)
+                 that.contents = response.data
+                 
+             })
+         }
+     },
+     created: function() {
+         this.getConfig()
+         }
+ }
+ 
+</script>
+
+<template>
+    <div style="margin: 20px; justify-content:space-around;">
+        <div style="display:flex"><h3> Config: </h3></div>
+        <div style="display:flex">
+            <ul>
+                <li v-for="item in contents" :key="item.name"> {{ item.name }} </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
+import SuiVue from 'semantic-ui-vue'
 import App from './App.vue'
 
+Vue.use(SuiVue);
 Vue.config.productionTip = false
 
 new Vue({
